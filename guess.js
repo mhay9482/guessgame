2ff36cc4633ef1ae84826f9112943808d3c0817b
mhay9482 <113653523+mhay9482@users.noreply.github.com>
2023-02-26 21:43:16 +1100

    Add files via upload

---
+++ guess.js
@@ -0,0 +1,23 @@
+let friendNumber;
+let guessForm = document.getElementById('guess-form');
+let resultDiv = document.getElementById('result');
+
+document.getElementById('friend-form').addEventListener('submit', function(event) {
+	event.preventDefault();
+	friendNumber = parseInt(event.target.elements['friend-number'].value);
+	alert('Your friend has picked their number. Now you can start guessing!');
+});
+
+guessForm.addEventListener('submit', function(event) {
+	event.preventDefault();
+	let guess = parseInt(guessForm.elements.guess.value);
+	if (!friendNumber) {
+		resultDiv.innerHTML = 'Please wait for your friend to pick their number.';
+	} else if (guess === friendNumber) {
+		resultDiv.innerHTML = 'Congratulations, you guessed the number!';
+	} else if (guess < friendNumber) {
+		resultDiv.innerHTML = 'Your guess is too low.';
+	} else {
+		resultDiv.innerHTML = 'Your guess is too high.';
+	}
+});
